44f1f09843252712a3df06e3f5d49445525c8809
wojak1488 <rizqiwicak@gmail.com>
2021-02-10 18:12:59 +0700

    new commit test

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var http = require('http');
 function on_Requets(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.write('Piss off !!');
+	response.write('Ok just a github test');
 	response.end();
-
-
 }
 
 http.createServer(on_Requets).listen(7777);
